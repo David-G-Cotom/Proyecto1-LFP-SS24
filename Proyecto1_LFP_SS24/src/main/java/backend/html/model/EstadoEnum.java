9c5ebbe93e9de3b0eaff3a19a7789b31f6caa887
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2024-09-20 12:52:50 -0600

    Agregando los Paquete con sus respectivas Clases para manejar los Analizadores Lexicos de cada Lenguaje por Separado

---
+++ Proyecto1_LFP_SS24/src/main/java/backend/html/model/EstadoEnum.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package backend.html.model;
+
+/**
+ *
+ * @author Carlos Cotom
+ */
+public enum EstadoEnum {
+    
+}
