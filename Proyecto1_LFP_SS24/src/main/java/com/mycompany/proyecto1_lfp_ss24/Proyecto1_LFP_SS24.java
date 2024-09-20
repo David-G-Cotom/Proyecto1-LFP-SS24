52a0f60160869baca7205129af1224dd22a559f1
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2024-09-20 12:23:23 -0600

    Agregando la Carpeta del Proyecto creada por NetBeans

---
+++ Proyecto1_LFP_SS24/src/main/java/com/mycompany/proyecto1_lfp_ss24/Proyecto1_LFP_SS24.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_lfp_ss24;
+
+/**
+ *
+ * @author Carlos Cotom
+ */
+public class Proyecto1_LFP_SS24 {
+
+    public static void main(String[] args) {
+        
+    }
+}
